2e9a206bc0bdf70c775b1c2ec5fe9ebb24fc1cbc
luis.chavez <ljosue.chavez@gmail.com>
2014-08-05 13:09:06 -0600

    anñadiendo bean controllar

---
+++ TesisAplication/src/java/com/tesis/administration/controller/TesisAdministrationIndexController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesis.administration.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Luis
+ */
+@ManagedBean
+@RequestScoped
+public class TesisAdministrationIndexController {
+
+    /** Creates a new instance of TesisAdministrationIndexController */
+    public TesisAdministrationIndexController() {
+    }
+}
